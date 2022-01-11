85e70d18697855ef35c37e84067648f2cd4f14e0
Paolo D'Onorio De Meo <p.donorio.demeo@gmail.com>
2022-01-11 22:43:20 +0100

    Test loop with bash script

---
+++ .github/scripts/send.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$3" ]; then
+    echo -e "\nMissing argument(s)!\n\nUsage:\n\$ $0 WORKFLOW BRANCH TARGET_ENV\n\n"
+    exit 1
+fi
+
+if [ -z "$TOKEN" ]; then
+    echo -e "Please define the variable \$TOKEN to authenticate to Github API\n\n"
+    exit 1
+fi
+
+WORKFLOW=$1
+BRANCH=$2
+TARGET_ENV=$3
+REPO="pdonorio/ga-meta-job"
+
+curl \
+    --request POST \
+    --url 'https://api.github.com/repos/${REPO}/actions/workflows/${WORKFLOW}/dispatches' \
+    --fail \
+    --header 'Content-Type: application/json' \
+    --header 'Authorization: Bearer ${TOKEN}' \
+    --header 'Accept: application/vnd.github.everest-preview+json' \
+    --data-raw '{
+        "ref": "${BRANCH}",
+        "inputs": {
+            "testkey": "42",
+            "target_env": "${TARGET_ENV}"
+        }
+    }'
